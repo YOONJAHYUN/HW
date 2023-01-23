49a0e74c77675616155fd577a38c851ad2ff13a4
YOONJAHYUN <bajh323@naver.com>
2023-01-23 17:18:14 +0900

    anagram done

--- 06/홈워크_6_2_new.py
+++ 06/홈워크_6_2_new.py
@@ -1,21 +1,41 @@
 def group_anagrams(words):
     # 하나의 단어에서 애너그램으로 만들 수 있는 모든 단어를 모은다.
-    # 딕셔너리 형태로 만들어서, 값들을 비교해 같은 값을 가진 key값들을 따로 list로 만들어준다.
+    # 딕셔너리 형태로 만들어서, value 값들 정렬해 서로 비교한다.
+    # value 값들을 정렬해서 같은 value를 찾는다.
+    # 같은 value끼리 anagram이 된다.
 
     new_words = {} # 빈 딕셔너리 만들기
     
     for word in words:
         # dic[key] = value : key 값에 value 넣기
         # 정렬된 형태로 넣어서 같은 값이 있는지 비교한다.
+       
         new_words[word] = sorted([word, word[0] + word[2] + word[1], word[1] + word[2] + word[0], word[1] + word[0] + word[2], word[2] + word[0] + word[1], word[2] + word[1] + word[0]])
+        
+     
+    # 큰 list 하나와 버릴 list 하나를 만들어서 버릴 list를 모아 큰 list에 집어넣는다.
+    anagram = []
 
-    print(new_words)
+    for i in new_words: # key값 순회
+        arr = []
+        for j in new_words: # key값과 key값들 순회
+            if new_words[i] == new_words[j]: # key값의 value들이 같으면
+                arr.append(j) # arr에 key 추가
+            else:
+                continue # 같지 않으면 반복
 
-    # value 값들이 같은 것들의 key끼리 묶는다.
-    # value를 순차적으로 돌려서 같은 걸 찾는다.
-    # anagram = []
-    # for 
+        if arr not in anagram: # arr 중복값 없애기 위해 큰 list에 추가
+            anagram.append(arr)
 
+    print(anagram)
+        
+    # 처음 아이디어는 key값을 value들 중에 있는지 없는지로 할려고 했는데, 하다보니까 그냥 value들끼리 비교하게 됐다.
+    # sorted를 써서 비교할거라면 애초부터 group_anagrams값을 sorted했으면 더 편했을 것 같다!
+
+
+
+
+    
 
 
 
--- 06/홈워크_python_06_hw02_P.py
+++ 06/홈워크_python_06_hw02_P.py
@@ -2,65 +2,15 @@ def group_anagrams(words):
     # 하나의 단어에서 애너그램으로 만들 수 있는 모든 단어를 모은다.
     # 딕셔너리 형태로 만들어서, 값들을 비교해 같은 값을 가진 key값들을 따로 list로 만들어준다.
     
-    new_words = {} # 빈 딕셔너리 만들기
+    alphabet = {}
+
     for word in words:
-        # dic[key] = value : key 값에 value 넣기
-        new_words[word] = [word[0] + word[2] + word[1], word[1] + word[2] + word[0], word[1] + word[0] + word[2], word[2] + word[0] + word[1], word[2] + word[1] + word[0]]
-    # print(new_words)
-
-    # value 값들만 모아서 key 값 하나하나와 비교해서 같은 것이 있는지 찾는다.
-    # anagram_words = []
-    # for new_word_list in new_words.values(): # value 값들만 추출하기
-    #         for new_word in new_word_list: # list안의 단어 하나씩 추출하기
-    #             for i in new_words.keys():
-    #                 if i == new_word:
-    #                     if i not in anagram_words:
-    #                         anagram_words += [i]
-    #                         print(anagram_words)
-
-    # for key in new_words:
-    #     for value in new_words[key]:
-    #         if key == value:
-    #             anagram_words.append(key)
-
-
-    #             print(anagram_words)
-
-    # value_list = new_words.values()
-    # print(value_list)
-    anagram_words = []
-    for key1 in new_words:
-        for key2 in new_words:
-            for value2 in new_words[key2]:
-                if key1 == value2:
-                    anagram_words += [[key1, key2]]
-                    
-                    print(anagram_words)
-
-
-            # if new_words == new_word:
-            #     print(new_words)
-
-    # for i in new_words.keys():
         
+        for i in word:
 
-# def group_anagrams(words):
-#     new_words = ()
-#     anagram_words = []
-#     for word in words:
-#         new_words[word] = [[word[0] + word[2] + word[1], word[1] + word[2] + word[0], word[1] + word[0] + word[2], word[2] + word[0] + word[1], word[2] + word[1] + word[0]]] 
-#     print(new_words)
-#     #     for new_word in new_words:
-#     #         if word == new_word:
-#     #             if word not in anagram_words:
-#     #                 anagram_words.append(word)
-#     # # anagram_words = []
-#     # # for word in words:
-#     # #     for new_word in new_words:
-#     # #         if word == new_word:
-#     # #             if word not in anagram_words:
-#     # #                 anagram_words.append(word)
-#     # print(anagram_words)
+            alphabet[i] = word.count(i)
+
+            print(alphabet)
 
 group_anagrams(['eat','tea','tan','ate','nat','bat'])
 
