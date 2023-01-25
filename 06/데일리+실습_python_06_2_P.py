6d85603bc34fbb471eae6b8b909de903c887abf5
YOONJAHYUN <dbswkgus323@gamil.com>
2023-01-25 16:58:51 +0900

    modified

--- 06/데일리+실습_python_06_2_P.py
+++ 06/데일리+실습_python_06_2_P.py
@@ -18,7 +18,23 @@ grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1
 
 # 메소드를 써보자
 
-grain_lst.sort(key = lambda x : x[1])
+grain_lst.sort(key = lambda x : x[1]) # 무엇을 기준으로 sort할건지 정할 수 있다.
+# 람다를 활용해서.... 특정 key를 이용해서
+# stack overflow 활용해서 찾아보장
 
 print(grain_lst[-1][0])
+
+
+# 교수님 풀이
+dict(grain_lst) # 딕셔너리형태 가능
+
+# 제일 비싼애의 index 번호를 찾는다.
+key = list(grain_lst.keys())
+value = list(grain_lst.values())
+
+max_value = max(value)
+print(max(value.index(max(value))))
+
+max_idx = value.index(max_value)
+print(key[max_idx])
 
